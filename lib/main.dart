5ec4755a2c641cf9b3f262aebc2fa72bf0156f1d
shdn <shdn.ab.alomar@gmail.com>
2024-02-07 16:30:21 +0300

    adding the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,22 +13,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(useMaterial3: true),
+      theme: ThemeData.dark(useMaterial3: true),
       home: HomePage(),
     ); // MaterialApp
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFE4E2DD),
+        title: Text(
+          "الملف الشخصي",
+          style: TextStyle(color: Color(0xFF292929)),
+        ),
+        centerTitle: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+      ),
       body: Column(
         children: [
-          CircleAvatar(
-            backgroundImage: AssetImage("images/profilePic.jpg"),
+          Container(
+            height: 150,
+            width: 150,
+            child: CircleAvatar(
+              backgroundImage: AssetImage("images/profilePic.jpg"),
+              radius: 100,
+            ),
           )
         ],
       ),
